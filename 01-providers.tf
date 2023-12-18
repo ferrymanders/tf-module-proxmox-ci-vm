cbda93683f1e31594bccf695b06cf101e6ace32d
Ferry Manders <ferrymanders@users.noreply.github.com>
2023-12-18 16:59:20 +0100

    Update 01-providers.tf

--- 01-providers.tf
+++ 01-providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
-      version = "~> 2.9.10"
+      source = "TheGameProfi/proxmox"
+      version = "~> 2.9.15"
     }
   }
 }
